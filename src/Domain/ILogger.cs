c55239494c5f7481fb47f2b0e5465e151cb6cc86
Anton Zimin <anton@saritasa.com>
2017-05-03 20:26:49 +0700

    Add another test

---
+++ src/Domain/ILogger.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain
+{
+    /// <summary>
+    /// Contains methods for logging.
+    /// </summary>
+    public interface ILogger
+    {
+        /// <summary>
+        /// Saves message to log.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        void Log(string message);
+    }
+}
---
+++ src/SimpleLogger.cs
@@ -0,0 +1,16 @@
+﻿using Domain;
+
+namespace InheritdocTest
+{
+    /// <summary>
+    /// Simple logger.
+    /// </summary>
+    public class SimpleLogger : ILogger
+    {
+        /// <inheritdoc />
+        public void Log(string message)
+        {
+            System.Console.WriteLine(message);
+        }
+    }
+}
